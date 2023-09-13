48ac245d6e2c5b07f659b75e22df4ef99c3dfb65
Egyedi Róbert <egyedirobika@gmail.com>
2023-09-13 11:20:21 +0200

    2ora

--- assets/js/js.js
+++ assets/js/js.js
@@ -13,8 +13,11 @@ function TagokFelvitele() {
             tagok:[]
         }
         lista.push(obj)
+        document.getElementById("diaknev").value = "";
+        document.getElementById("diaklakohely").value = "";
         document.getElementById("osztalyfelvetel").style.display = "none";
         document.getElementById("tagfelvetel").style.display = "block";
+        document.getElementById("diaknev").focus()        
     }
 }
 
@@ -33,4 +36,31 @@ function Mentes() {
         document.getElementById("diaklakohely").value = ""
         document.getElementById("diaknev").focus()
     }
+}
+
+function Kilepes() {
+    document.getElementById("osztalynev").value = "";
+    document.getElementById("osztalyfonoknev").value = "";
+    document.getElementById("osztalyfelvetel").style.display = "block";
+    document.getElementById("tagfelvetel").style.display = "none";
+}
+
+function Lista() 
+{
+    document.getElementById("osztalyfelvetel").style.display = "none";
+
+    var s = "";
+    for (let i = 0; i < lista.length; i++) {
+        s += "<h3>"+lista[i].osztalynev+"oszt.f.:"+lista[i].osztalyfonoknev+"</h3>"
+        s += "<ol>"
+
+        for (let j = 0; j <lista[i].tagok.length; j++) {
+            s += "<li>"+lista[i].tagok[j].diaknev+", "+lista[i].tagok[j].lakohely+"</li>"
+        }
+        s += "</ol>"
+    }
+
+    
+    document.getElementById("listanezet").innerHTML = s;   
+    document.getElementById("listanezet").style.display = "block";   
 }
